509da24bc9cf868c9029297c5b54426e7ede33ae
Nick Renieris <velocityra@gmail.com>
2019-10-25 19:32:08 +0300

    Update to Boost 1.71.0

--- build.cmd
+++ build.cmd
@@ -4,10 +4,17 @@ bcp ^
     system ^
 	optional ^
 	^
+	log ^
 	config ^
 	predef ^
     bootstrap.bat ^
     bootstrap.sh ^
     boostcpp.jam ^
     boost-build.jam ^
+    tools/build ^
+    tools/boost_install/BoostConfig.cmake ^
+    tools/boost_install/BoostDetectToolset.cmake ^
+    tools/boost_install/boost-install.jam ^
+    tools/boost_install/BoostDetectToolset.cmake ^
+    tools/boost_install/boost-install-dirs.jam ^
     --boost="%1" .
