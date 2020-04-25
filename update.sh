fa7fbdc17ebbb8ec983ca4ef269e265d8fac537c
ichisadashioko <ichisadashioko@gmail.com>
2020-04-26 01:45:48 +0700

    update boost to 1.72.0 (#2)
    - update extracting boost subset instruction
    - add extracting script for Linux

---
+++ update.sh
@@ -0,0 +1,32 @@
+./bootstrap.sh
+./b2 tools/bcp
+
+SUBSETS_OUTDIR=./subsets-boost
+
+if [ -f "$SUBSETS_OUTDIR" ]; then
+    rm "$SUBSETS_OUTDIR"
+elif [ -d "$SUBSETS_OUTDIR" ]; then
+    rm -r "$SUBSETS_OUTDIR"
+fi
+
+echo "Creating $SUBSETS_OUTDIR"
+mkdir -p "$SUBSETS_OUTDIR"
+
+./dist/bin/bcp \
+filesystem \
+program_options \
+system \
+optional \
+log \
+config \
+predef \
+bootstrap.bat \
+bootstrap.sh \
+boostcpp.jam \
+boost-build.jam \
+tools/build \
+tools/boost_install/BoostConfig.cmake \
+tools/boost_install/BoostDetectToolset.cmake \
+tools/boost_install/boost-install.jam \
+tools/boost_install/boost-install-dirs.jam \
+"$SUBSETS_OUTDIR"
